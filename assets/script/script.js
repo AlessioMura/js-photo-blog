ee03d2e11f6988f77849025a34c58cdb5f9c50ab
AlessioMura <alessiomura03@gmail.com>
2024-10-18 11:45:29 +0200

    added js script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,24 @@
-console.log('hi');
+
+let card_el = document.querySelector('.container');
+
+axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+    .then(response => {
+        const cards = response.data;
+
+        let card_elements = '';
+        cards.forEach(card => {
+            const { title, url} = card;
+
+            const markup = 
+            `
+            <div class="card col-4">
+                <img class="pin" src="./assets/img/pin.svg" alt="">
+                <img class="card_img" src="${url}" alt="">
+                <p class="card_txt">${title}</p>
+            </div>
+            `;
+
+            card_elements += markup;
+        });
+        card_el.innerHTML = card_elements
+    })
