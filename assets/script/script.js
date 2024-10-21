9126a4a427f0d933198b11a8c307b0546753f723
AlessioMura <alessiomura03@gmail.com>
2024-10-21 13:15:10 +0200

    milestone 3

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,25 +7,38 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
 
         let card_elements = '';
         cards.forEach(card => {
-            
+
             const { title, url } = card;
 
             const markup =
                 `
             <div class="card col-4 col-md-6 col-sm">
                 <img class="pin" src="./assets/img/pin.svg" alt="">
-                <img class="card_img" onclick="on()" src="${url}" alt="">
+                <img class="card_img" src="${url}" alt="">
                 <p class="card_txt">${title}</p>
             </div>
             `;
-            
+
             card_elements += markup;
-            
         });
         card_el.innerHTML = card_elements
 
+        const images = document.querySelectorAll('.card_img');
+
+        images.forEach(image => {
+            image.addEventListener('click', function () {
+                const url = this.src;
+                const overlay = document.getElementById('overlay');
+                const overlayImage = document.getElementById('overlay_img');
+
+                overlayImage.src = url;
+                overlay.style.display = 'flex';
+            });
+        });
     })
 
-on = () => document.getElementById("overlay").style.display = "flex"
-    
-off = () => document.getElementById("overlay").style.display = "none";
+function closeOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+}
+document.getElementById('close-btn').addEventListener('click', closeOverlay);
