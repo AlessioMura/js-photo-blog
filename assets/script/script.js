89f3f47a4aed5775fdf5ffa978dc85c7a2687a38
AlessioMura <alessiomura03@gmail.com>
2024-10-21 10:56:26 +0200

    milestone 2

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,18 +7,25 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
 
         let card_elements = '';
         cards.forEach(card => {
-            const { title, url} = card;
+            
+            const { title, url } = card;
 
-            const markup = 
-            `
+            const markup =
+                `
             <div class="card col-4 col-md-6 col-sm">
                 <img class="pin" src="./assets/img/pin.svg" alt="">
-                <img class="card_img" src="${url}" alt="">
+                <img class="card_img" onclick="on()" src="${url}" alt="">
                 <p class="card_txt">${title}</p>
             </div>
             `;
-
+            
             card_elements += markup;
+            
         });
         card_el.innerHTML = card_elements
+
     })
+
+on = () => document.getElementById("overlay").style.display = "flex"
+    
+off = () => document.getElementById("overlay").style.display = "none";
